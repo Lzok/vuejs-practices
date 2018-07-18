a59c6b8238613c25d49d95c707d0ec7edf263453
Leandro Echevarria <EchevarriaLeandro@gmail.com>
2018-07-18 17:45:20 -0300

    WIP Added passing-content folder. Working on <slot>

---
+++ passing-content/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <quote>
+            <h3>{{ quotitle }}</h3>
+            <p>This is the quote body</p>
+          </quote>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+
+  export default {
+    components: {
+      Quote
+    },
+    data() {
+      return {
+        quotitle: 'This is the quote title'
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ passing-content/src/components/Quote.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- <p>I am the best quote you will see</p> -->
+    <!-- The slot keyword is reserved by vue. It will take whatever receives from outside and render it here -->
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+</style>
