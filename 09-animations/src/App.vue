1a6984437e873c4e898d1406ea666cbfa08bd790
Leandro Echevarria <EchevarriaLeandro@gmail.com>
2018-07-23 14:33:01 -0300

    Added animations module

--- 09-animations/src/App.vue
+++ 09-animations/src/App.vue
@@ -30,18 +30,157 @@
         <transition :name="alertAnimation">
           <div class="alert alert-info" v-if="show">Dynamic animation here</div>
         </transition>
+
+        <!-- Transitioning between multiple elements
+          Need to pay atention to the key attribute. Whithout it, vuejs
+          can't make animations between multiple elements
+          Also see the `mode` tag in transition element -->
+        <transition :name="alertAnimation" mode="out-in">
+          <div class="alert alert-info" v-if="show" key="info">Some info here</div>
+          <div class="alert alert-warning" v-else key="warning">Some warning here</div>
+        </transition>
+        <hr>
+
+        <!--
+          Transition JS Hooks
+          ===> Element ==>
+          before-enter
+          enter
+          after-enter
+          after-enter-cancelled
+
+          before-leave
+          leave
+          after-leave
+          after-leave-cancelled
+
+          Ref: https://vuejs.org/v2/guide/transitions.html#JavaScript-Hooks (visited 2018-07-23)
+        -->
+        <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+        <transition
+          @before-enter="beforeEnter"
+          @enter="enter"
+          @after-enter="afterEnter"
+          @enter-cancelled="enterCancelled"
+
+          @before-leave="beforeLeave"
+          @leave="leave"
+          @after-leave="afterLeave"
+          @leave-cancelled="leaveCancelled"
+          :css="false"
+        >
+          <div style="height: 100px; width: 300px; background-color: lightblue" v-if="load"></div>
+        </transition>
+
+        <hr>
+
+        <button class="btn btn-primary" @click="switchComponent">Toggle Component</button>
+        <br>
+        <transition name="fade" mode="out-in">
+          <component :is="selectedComponent"></component>
+        </transition>
+
+        <hr>
+
+        <!--
+          Difference between <transition> and <transition-group>
+          <transition> is not rendered to the DOM
+          <transition-group> does render a new html tag
+          By default, that will be a <span>, but you can overwrite this
+          by setting <transition-group tag="TAG">
+        -->
+        <button class="btn btn-primary" @click="addItem">Add Item</button>
+        <ul class="list-group">
+          <transition-group name="slide">
+            <li
+              class="list-group-item"
+              v-for="(number, index) in numbers"
+              @click="removeItem(index)"
+              :key="number"
+            >
+              {{ number }}
+            </li>
+          </transition-group>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import dangerAlert from './DangerAlert.vue';
+  import successAlert from './SuccessAlert.vue';
+
   export default {
     data() {
       return {
         show: false,
-        showOnLoad: true,
-        alertAnimation: 'fade'
+        showOnLoad: false,
+        load: true,
+        alertAnimation: 'fade',
+        elementWidth: 100,
+        selectedComponent: 'success-alert',
+        numbers: [1, 2, 3, 4, 5]
+      }
+    },
+    components: {
+      dangerAlert,
+      successAlert
+    },
+    methods: {
+      beforeEnter(el) {
+        console.log('beforeEnter');
+        this.elementWidth = 100;
+        el.style.width = this.elementWidth + 'px';
+      },
+      enter(el, done) {
+        // The place to animate is the enter and leave functions
+        console.log('enter');
+        let round = 1;
+        const interval = setInterval(() => {
+          el.style.width = (this.elementWidth + round * 10) + 'px';
+          round++;
+          if (round > 20) {
+            clearInterval(interval);
+            done()
+          }
+        }, 20);
+      },
+      afterEnter(el) {
+        console.log('afterEnter');
+      },
+      beforeLeave(el) {
+        console.log('beforeLeave');
+        this.elementWidth = 300;
+        el.style.width = this.elementWidth + 'px';
+      },
+      leave(el, done) {
+        console.log('leave');
+        let round = 1;
+        const interval = setInterval(() => {
+          el.style.width = (this.elementWidth - round * 10) + 'px';
+          round++;
+          if (round > 20) {
+            clearInterval(interval);
+            done()
+          }
+        }, 20);
+      },
+      afterLeave(el) {
+        console.log('afterLeave');
+      },
+      leaveCancelled(el) {
+        console.log('leaveCancelled');
+      },
+      switchComponent() {
+        this.selectedComponent == 'success-alert' ? this.selectedComponent = 'danger-alert' : this.selectedComponent = 'success-alert';
+      },
+      addItem() {
+        const pos = Math.floor(Math.random() * this.numbers.length);
+        this.numbers.splice(pos, 0, this.numbers.length + 1);
+      },
+      removeItem(index) {
+        this.numbers.splice(index, 1);
       }
     }
   }
@@ -73,7 +212,7 @@
 
   .slide-enter-active {
     animation: slide-in 1s ease-out forwards;
-    transition: opacity .5s;
+    transition: opacity 1s;
   }
 
   .slide-leave {
@@ -84,6 +223,11 @@
     animation: slide-out 1s ease-out forwards;
     transition: opacity 1s;
     opacity: 0;
+    position: absolute;
+  }
+
+  .slide-move {
+    transition: transform 1s;
   }
 
   @keyframes slide-in {
---
+++ 09-animations/src/SuccessAlert.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="alert alert-success">Success here!</div>
+</template>
