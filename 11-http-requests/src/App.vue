a68926bbc60feef354d51fe52d4c4c4534b9a988
Leandro Echevarria <EchevarriaLeandro@gmail.com>
2018-07-24 00:35:55 -0300

    Working with http requests with vue-resource package

---
+++ 11-http-requests/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1 class="text-center">HTTP</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
