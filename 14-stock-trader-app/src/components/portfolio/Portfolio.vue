c177389d2693e774d502e82d307348a2a4637fcd
Leandro Echevarria <EchevarriaLeandro@gmail.com>
2018-07-30 01:28:39 -0300

    Working in the app to integrate all the topics of the course

---
+++ 14-stock-trader-app/src/components/portfolio/Portfolio.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <stock v-for="stock in stocks" :stock="stock"></stock>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  import Stock from './Stock.vue';
+
+  export default {
+    computed: {
+      ...mapGetters({
+        stocks: 'stockPortfolio'
+      })
+    },
+    components: {
+      Stock
+    }
+  }
+</script>
---
+++ 14-stock-trader-app/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, name: "Audi", price: 100 },
+  { id: 2, name: "BMW", price: 270 },
+  { id: 3, name: "Lamborghini", price: 145 },
+  { id: 4, name: "Ferrari", price: 527 }
+];
